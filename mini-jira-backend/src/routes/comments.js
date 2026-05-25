2f6b6bf5563aa4ec42c79a59a452fc7c81b0a21b
nouraashrafbedir@gmail.com <nouraashrafbedir@gamil.com>
2026-05-26 00:18:27 +0300

    fixes

--- mini-jira-backend/src/routes/comments.js
+++ mini-jira-backend/src/routes/comments.js
@@ -4,6 +4,5 @@ const commentController = require('../controllers/commentController');
 const { authMiddleware, requireRole } = require('../middleware/auth');
 
 router.get('/:taskId', authMiddleware, commentController.getCommentsByTaskId);
-router.post('/:taskId', authMiddleware, commentController.createComment);
-
+router.post('/:taskId', authMiddleware, requireRole(['Manager', 'Employee']), commentController.createComment);
 module.exports = router;
